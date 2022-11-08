1e7e1a492c2d7e862758f0d5e3b8ec6def5ef084
hukangkang <hukangkang@xiaomi.com>
2022-11-08 14:55:03 +0800

    init

---
+++ application/src/main/java/application/Application.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Application {
+
+    // java -javaagent:D:\study\java-instrumentation\agent\target\agent-1.0-SNAPSHOT.jar -jar D:\study\java-instrumentation\age
+    // nt\target\application-1.0-SNAPSHOT.jar
+    public static void main(String[] args) {
+        System.out.println("main start");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main end");
+    }
+}
