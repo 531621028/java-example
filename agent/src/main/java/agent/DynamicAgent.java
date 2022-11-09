40cc5d99b1187f30a9163d7e210ecc0e30de5de1
hukangkang <hukangkang@xiaomi.com>
2022-11-09 10:45:33 +0800

    动态加载配置文件

--- agent/src/main/java/agent/DynamicAgent.java
+++ agent/src/main/java/agent/DynamicAgent.java
@@ -1,12 +1,16 @@
 package agent;
 
 import java.lang.instrument.Instrumentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DynamicAgent {
 
+    // 日志的依赖也必须被绑定的JVM进程的应用的classpath中，打印的何时
+    private static final Logger log = LoggerFactory.getLogger(DynamicAgent.class);
 
     public static void agentmain(String agentArgs, Instrumentation instrumentation) {
-        // 此处的代码运行在被绑定的JVM进程上，也就是在Application的进程中输出attach JVM success
-        System.out.println("attach JVM success");
+        // 此处的代码运行在被绑定的应用的JVM进程上，也就是在Application的进程中输出attach JVM success，所以相关的依赖和配置是被绑定应用数据
+        log.info("attach JVM success");
     }
 }
--- application/src/main/java/application/Application.java
+++ application/src/main/java/application/Application.java
@@ -7,7 +7,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("main start");
         try {
-            Thread.sleep(10000);
+            while (true) {
+                Thread.sleep(1000);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
